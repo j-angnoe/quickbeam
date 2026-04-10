1b5e99da83f7a48a1d2d6c2580227cf4aa52bb5f
Danila Poyarkov <dev@dannote.net>
2026-04-10 15:01:27 +0300

    Release v0.9.0

--- checksum-QuickBEAM.Native.exs
+++ checksum-QuickBEAM.Native.exs
@@ -1,6 +1,4 @@
 [
-  {"Elixir.QuickBEAM.Native-v0.8.1-x86_64-linux-gnu.so.tar.gz",
-   "sha256:f651195a54cebaad0e1f36116c4efd3c98a898c13e54cdafe43a195e576a135b"},
-  {"Elixir.QuickBEAM.Native-v0.8.1-aarch64-macos-none.so.tar.gz",
-   "sha256:c658f2b932d90a9aab37fa18c39e8c5ec1392dea220ff08e9762bf03ad846bf9"}
+  {"Elixir.QuickBEAM.Native-v0.9.0-x86_64-linux-gnu.so.tar.gz", "sha256:TODO"},
+  {"Elixir.QuickBEAM.Native-v0.9.0-aarch64-macos-none.so.tar.gz", "sha256:TODO"}
 ]
