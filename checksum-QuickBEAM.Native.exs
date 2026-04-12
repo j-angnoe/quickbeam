d8194d37b8dcae354d0d2abf742110d2e021c975
Joshua <joshua@fluxfx.nl>
2026-04-12 16:54:31 +0200

    Fix checksums to match GitHub Actions built artifacts

--- checksum-QuickBEAM.Native.exs
+++ checksum-QuickBEAM.Native.exs
@@ -1,8 +1,8 @@
 [
   {"Elixir.QuickBEAM.Native-v0.9.0-x86_64-linux-gnu.so.tar.gz",
-   "sha256:fd0e5085a62e9c79e54c1c76411363a6015b3f3a8ec777c9a3c7d35dba6a6248"},
+   "sha256:2c5d6e59cd775bd56a0e0d85be67a513acc3ec00353a32b338d3f6adbe1ac2f2"},
   {"Elixir.QuickBEAM.Native-v0.9.0-aarch64-linux-gnu.so.tar.gz",
-   "sha256:2d5ba5088f8f4e2d2dcc3163b60837707d0e3a0ffaec21e2c0c4f04c1bdd5c98"},
+   "sha256:72e85558a2bea285d37fb0d41c24d8f5c74df35cbe971d79053b4655ba419d1c"},
   {"Elixir.QuickBEAM.Native-v0.9.0-aarch64-macos-none.so.tar.gz",
-   "sha256:29a1d8e87277517a81d0da3c2482d6e4d6947be17377b130bfaee0f34dd49c7a"}
+   "sha256:807c61b7a5deb09027814aa1cba8213f88c008002b9be625418bd57cd22f7375"}
 ]
