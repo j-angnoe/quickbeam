64fffea83ca698a7913502198f3ca8f525ed18b1
Joshua <joshua@fluxfx.nl>
2026-04-12 16:44:17 +0200

    Update checksums for Debian 12 compatible precompiled NIFs

--- checksum-QuickBEAM.Native.exs
+++ checksum-QuickBEAM.Native.exs
@@ -1,6 +1,8 @@
 [
   {"Elixir.QuickBEAM.Native-v0.9.0-x86_64-linux-gnu.so.tar.gz",
-   "sha256:a2de16b96dff782c2261592b9db50f12f122ed783bb90f405d4c02e54e43cdce"},
+   "sha256:fd0e5085a62e9c79e54c1c76411363a6015b3f3a8ec777c9a3c7d35dba6a6248"},
+  {"Elixir.QuickBEAM.Native-v0.9.0-aarch64-linux-gnu.so.tar.gz",
+   "sha256:2d5ba5088f8f4e2d2dcc3163b60837707d0e3a0ffaec21e2c0c4f04c1bdd5c98"},
   {"Elixir.QuickBEAM.Native-v0.9.0-aarch64-macos-none.so.tar.gz",
-   "sha256:5021ae793929a721d9d4f349e71c72760edd2320bf8805fa541ab63eb6eca791"}
+   "sha256:29a1d8e87277517a81d0da3c2482d6e4d6947be17377b130bfaee0f34dd49c7a"}
 ]
